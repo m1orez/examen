84f6a468ac484afa8fd4554f556dfde0331e5b33
M1orez <pauline@walhout-hia.nl>
2026-05-19 15:58:10 +0200

    google maps embed

--- info.php
+++ info.php
@@ -15,8 +15,9 @@
         <a class="navButtons" href="./login.php">Inloggen</a>
     </nav>
     <section>
-        <h1>Regels</h1>
+        <h1>Over de kuip</h1>
         <p></p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2462.211817698291!2d4.522609677233101!3d51.893598482416444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c433cd309b5947%3A0x4526fb859b254c51!2sDe%20kuip!5e0!3m2!1snl!2snl!4v1779199043649!5m2!1snl!2snl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </section>
 </body>
 </html>
